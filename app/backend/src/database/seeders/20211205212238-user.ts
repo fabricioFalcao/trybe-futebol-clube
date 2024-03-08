608906df1a1fe785b2d47248d4d51a2aaad57d48
fabricioFalcao <fabriciohfd@gmail.com>
2024-03-08 12:02:18 -0300

    Migrations and seeders for users table

--- app/backend/src/database/seeders/20211205212238-user.ts
+++ app/backend/src/database/seeders/20211205212238-user.ts
@@ -8,22 +8,22 @@ export default {
         role: 'admin',
         email: 'admin@admin.com',
         password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
-          // senha: secret_admin
+        // senha: secret_admin
       },
       {
         username: 'User',
         role: 'user',
         email: 'user@user.com',
-        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
-          // senha: secret_user
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+        // senha: secret_user
       },
       // os logins abaixo são intencionalmente inválidos, pois serão usados nos testes
       {
         username: 'User',
         role: 'user',
         email: '@user.com',
-        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
-          // senha: secret_user
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+        // senha: secret_user
       },
       {
         username: 'User',
